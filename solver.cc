0713d07d3c129d5e1564ef62a4a7ddf642d9fb9c
lucarp <lucarp@lucarp.com>
2018-11-21 19:15:09 +0100

    first commit

---
+++ solver.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+    vector<vector<int>> variables = {{0},{1,2},{1,2},{0,1,2},{1}};
+    vector<vector<int>> clauses = {{0,3},{2,-3,-4},{-1,-2,-3}};
+
+
+    return 1;
+}
+
+
